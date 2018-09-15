b2c9cfb1501a13701266e87c49f12fb71ec084e7
Edgar Catalan <ecatala6@mail.ccsf.edu>
2018-09-15 15:51:09 -0700

    Added bootstrap css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'sup ladiessss' });
+  res.render('index', { title: 'The View' });
 });
 
 module.exports = router;
