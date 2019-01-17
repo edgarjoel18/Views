473278a86ccf65d60e6c100de1c5e2af7271f600
Edgar Catalan <ecatala6@mail.ccsf.edu>
2019-01-16 19:09:52 -0800

    started category model

--- migrations/20181220031206-create-category.js
+++ migrations/20181220031206-create-category.js
@@ -1,17 +1,14 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Posts', {
+    return queryInterface.createTable('Categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      title: {
-        type: Sequelize.STRING
-      },
-      body: {
+      name: {
         type: Sequelize.STRING
       },
       createdAt: {
@@ -25,6 +22,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Posts');
+    return queryInterface.dropTable('Categories');
   }
 };
