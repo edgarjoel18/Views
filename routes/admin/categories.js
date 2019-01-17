cbcd17bac25633bd38f03b78a60674db12c14cae
Francis Li <mail@francisli.com>
2019-01-16 19:32:00 -0800

    Admin category editing

---
+++ routes/admin/categories.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../../models');
+var moment = require('moment');
+
+router.get('/', function(req, res, next) {
+  models.Category.all({
+    order: ['name']
+  }).then(function(categories) {
+    res.render('admin/categories/index', {
+      layout: 'admin/layout',
+      title: 'Categories',
+      categories: categories,
+      moment: moment,
+      dateFormat: 'YYYY-MM-DD HH:mm'
+    });
+  });
+});
+
+router.post('/', function(req, res, next) {
+  models.Category.create({
+    name: req.body.name,
+  }).then(function(category) {
+    req.flash('info', 'Category created!');
+    res.redirect(`/admin/categories/${category.id}/edit`);
+  });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('admin/categories/new', {
+    layout: 'admin/layout',
+    title: 'New Category',
+    category: models.Category.build(),
+  });
+});
+
+router.get('/:id/edit', function(req, res, next){
+  models.Category.findById(req.params.id).then(function(category){
+    res.render('admin/categories/edit', {
+      layout: 'admin/layout',
+      title: 'Edit Category',
+      category: category,
+      moment: moment
+    });
+  });
+});
+
+router.patch('/:id', function(req, res, next){
+  models.Category.findById(req.params.id).then(function(category) {
+    category.update({
+      name: req.body.name,
+    }) .then(function(){
+      req.flash('info', 'Category saved!');
+      res.redirect(`/admin/categories/${category.id}/edit`);
+    });
+  });
+});
+
+module.exports = router;
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var articlesRouter = require('./articles');
+var categoriesRouter = require('./categories');
 
 router.use('/articles', articlesRouter);
+router.use('/categories', categoriesRouter);
 
 router.get('/', function(req, res, next) {
   res.redirect('/admin/articles');
