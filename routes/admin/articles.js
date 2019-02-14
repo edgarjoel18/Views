53fd58ed29dce1ccfe9eeb6e80054e59693bd83e
Francis Li <mail@francisli.com>
2019-02-13 20:58:32 -0800

    Make picture singular in url paths

--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -26,7 +26,7 @@ router.get('/', function(req, res, next) {
 
 function handlePicture(req, article, callback) {
   if (req.files && req.files.picture) {
-    const key = `articles/pictures/${uuid()}/original.${mime.extension(req.files.picture.mimetype)}`;
+    const key = `articles/picture/${uuid()}/original.${mime.extension(req.files.picture.mimetype)}`;
     if (process.env.AWS_S3_BUCKET) {
       var s3 = new AWS.S3({
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
