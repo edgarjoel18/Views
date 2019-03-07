7a8f264fd0406d80735e14e54e2c0dfab9e5bd75
Edgar Catalan <ecatala6@mail.ccsf.edu>
2019-03-06 19:22:31 -0800

    Update on wed 3/6

---
+++ public/javascripts/commentButton.js
@@ -0,0 +1,17 @@
+function commentButton(){
+
+  var x = document.getElementById("commentBox");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+
+}
+
+
+
+// <div>
+// <button type="button" class="btn btn-dark commentbutton"> Comment</button>
+//
+// </div>
--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,14 +1,8 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
-  
+
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
- 
-                    
-  
-  
-  
-  
   
